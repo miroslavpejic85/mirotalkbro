fd1fa26d38a59245cc96621150fa93bbe17e0bfb
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2025-01-21 00:44:55 +0100

    [mirotalkbro] - improve css

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -10,7 +10,7 @@ console.log('Viewer', {
 
 const body = document.querySelector('body');
 
-const waitingBroadcaster = document.getElementById('waitingBroadcaster');
+const awaitingBroadcaster = document.getElementById('awaitingBroadcaster');
 const viewerForm = document.getElementById('viewerForm');
 const viewerFormHeader = document.getElementById('viewerFormHeader');
 const viewerButtons = document.getElementById('viewerButtons');
@@ -122,7 +122,7 @@ socket.on('offer', async (id, description, iceServers) => {
     peerConnection.ontrack = (event) => {
         saveRecording();
         attachStream(event.streams[0]);
-        hideElement(waitingBroadcaster);
+        hideElement(awaitingBroadcaster);
     };
 
     peerConnection.onicecandidate = (event) => {
