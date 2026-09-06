09320bdd0d0042c271a43217aea55fdd76b6fc10
Miroslav Pejic <miroslav.pejic.85@gmail.com>
2026-09-06 15:11:06 +0200

    [mirotalkbro] - fix typo

--- public/js/config.js
+++ public/js/config.js
@@ -5,7 +5,7 @@ const homePage = {
     showCopyright: true,
     about: {
         show: true,
-        url: 'https://docs.mirotalk.com/sites/bro.html',
+        url: 'https://docs.mirotalk.com/sites/bro',
     },
     buttons: {
         broadcast: true,
